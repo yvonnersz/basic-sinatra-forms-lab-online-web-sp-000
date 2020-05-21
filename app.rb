7a30e32b4089837a9d8cc28a763748beb97475d7
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-21 19:01:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     erb :newteam
   end
 
-  post '/newteam' do
+  post '/team' do
     @team_name = params["name"]
     @coach = params["coach"]
     @pg = params["pg"]
@@ -15,11 +15,8 @@ class App < Sinatra::Base
     @pf = params["pf"]
     @center = params["center"]
 
-    erb :newteam
-  end
-
-  post '/team' do
     erb :team
   end
+
 
 end
